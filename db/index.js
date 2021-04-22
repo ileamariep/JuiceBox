f20f7cdfc60f1f360e5f2b75d07d88af860b9f6c
ilea <ileamarie@gmail.com>
2021-04-22 14:45:43 -0500

    Part 2 of Part 2 Complete

--- db/index.js
+++ db/index.js
@@ -315,6 +315,20 @@ async function getPostsByTagName(tagName) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username]);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
 module.exports = {
   client,
   createUser,
@@ -331,4 +345,5 @@ module.exports = {
   getPostById,
   getPostsByTagName,
   getAlltags,
+  getUserByUsername,
 };
--- index.js
+++ index.js
@@ -1,28 +1,22 @@
+require("dotenv").config();
+
+
 const PORT = 3000;
-const express = require('express');
+const express = require("express");
 const server = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const { client } = require('./db');
 
-const apiRouter = require('./api');
-
-server.use('/api', apiRouter);
-
-server.use(bodyParser.json());
-
-server.use(morgan('dev'));
-
-server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
-
+const { client } = require("./db");
 client.connect();
 
+const bodyParser = require("body-parser");
+server.use(bodyParser.json());
+
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
