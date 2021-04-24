d64716209bd964200815380e6fe88030ebc0b933
ilea <ileamarie@gmail.com>
2021-04-24 11:24:25 -0500

    updated client on index db

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const { PORT = 3000 } = process.env
 const express = require("express");
 const server = express();
 
-// const { client } = require("./db");
+const { client } = require("./db");
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-});
+
 
 client.connect();
 
